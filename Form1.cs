340eea4d719cd5c9f81708ab01094fb6a19d61f5
mennan <mennankose@gmail.com>
2011-05-18 20:32:36 +0300

    İlk yükleme

---
+++ Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace AutoPoke
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Login
+
+            string result = PostClass.HttpRequest("http://m.facebook.com/login.php", "email=mennan.kose&pass=fenerbahce1907");
+
+            if (result.Contains("inputpassword"))
+                MessageBox.Show("Kullanıcı adı veya şifre hatalı");
+            else
+                MessageBox.Show("Giriş yapıldı.");
+            
+            Regex rg = new Regex("poke=(.*?)&amp;");
+            MatchCollection m = rg.Matches(result);
+
+            List<string> UserID = new List<string>();
+
+            foreach (Match item in m)
+            {
+                string r = item.Groups[0].ToString();
+                string[] id = r.Split('=');
+                id = id[1].Split('&');
+
+                UserID.Add(id[0]);
+            }
+
+            foreach (string item in UserID)
+            {
+                PostClass.HttpRequest("http://www.facebook.com/ajax/poke.php?__a=1", String.Format("post_form_id=7a0fb076716c78523c08d48672af3f3f&uid={0}&pokeback=1&opp=&pk01=Poke&__d=1&fb_dtsg=kVdpn&lsd&post_form_id_source=AsyncRequest", item));
+            }
+        }
+    }
+}
---
+++ PostClass.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+
+namespace AutoPoke
+{
+    public class PostClass
+    {
+        private static CookieContainer cc = new CookieContainer();
+
+        public static string HttpRequest(string Address, string Data)
+        {
+            string source;
+
+            string strBuffer = Data;
+
+            byte[] buffer = System.Text.Encoding.ASCII.GetBytes(strBuffer);
+
+            HttpWebRequest WebReq = (HttpWebRequest)WebRequest.Create(Address);
+            WebReq.CookieContainer = cc;
+            WebReq.Method = "POST";
+
+            WebReq.UserAgent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3";
+
+            WebReq.ContentType = "application/x-www-form-urlencoded";
+            WebReq.ContentLength = buffer.Length;
+            WebReq.Referer = "http://www.facebook.com/";
+            WebReq.KeepAlive = true;
+
+            WebReq.AllowAutoRedirect = true;
+
+
+            Stream PostData = WebReq.GetRequestStream();
+            PostData.Write(buffer, 0, buffer.Length);
+            PostData.Close();
+
+            HttpWebResponse response = (HttpWebResponse)WebReq.GetResponse();
+
+            cc.SetCookies(WebReq.Address, "Set-Cookie");
+
+            foreach (string cookie in response.Headers.AllKeys)
+            {
+                string val = response.Headers[cookie].ToString();
+                response.Cookies.Add(new Cookie(cookie, val));
+            }
+
+            using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+            {
+                source = reader.ReadToEnd();
+            }
+
+            return source;
+        }
+
+        //UserID uzunluğunu alan function
+        public static int StrCount(string text, string pattern)
+        {
+            int num = 0;
+            int startIndex = 0;
+            while ((startIndex = text.IndexOf(pattern, startIndex)) != -1)
+            {
+                startIndex += pattern.Length;
+                num++;
+            }
+            return num;
+        }
+
+        //UserID alanını text'ten ayıran function
+        public static string Split(string msg, string delim, int part)
+        {
+            try
+            {
+                for (int i = 0; i < part; i++)
+                {
+                    msg = msg.Substring(msg.IndexOf(delim) + delim.Length);
+                }
+                if (msg.IndexOf(delim) == -1)
+                {
+                    return msg;
+                }
+                return msg.Substring(0, msg.IndexOf(delim));
+            }
+            catch
+            {
+                return "";
+            }
+        }
+    }
+}
